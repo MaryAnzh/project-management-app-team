2e23206cb07df83fc95dce313cc5394188d76036
MaryAnzh <MaryAnzh@mail.ru>
2022-04-27 12:52:07 +0400

    feat: add modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'project-management', loadChildren: () => import('./project-management/project-management.module').then(m => m.ProjectManagementModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
