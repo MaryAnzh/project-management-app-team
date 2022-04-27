aff325772e33755838fe4a4341de69e8fcf08ec4
Mowgle88 <mowgle1988@gmail.com>
2022-04-27 21:50:13 +0300

    feat: add login-page and registration-page components

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { AuthComponent } from './auth.component';
-
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
 @NgModule({
-  declarations: [
-    AuthComponent
-  ],
+  declarations: [LoginPageComponent, RegistrationPageComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    RouterModule.forChild([
+      { path: 'login', component: LoginPageComponent },
+      { path: 'registration', component: RegistrationPageComponent },
+    ]),
+  ],
+  exports: [RouterModule],
 })
 export class AuthModule { }
--- src/app/auth/registration-page/registration-page.component.spec.ts
+++ src/app/auth/registration-page/registration-page.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AuthComponent } from './auth.component';
+import { RegistrationPageComponent } from './registration-page.component';
 
-describe('AuthComponent', () => {
-  let component: AuthComponent;
-  let fixture: ComponentFixture<AuthComponent>;
+describe('RegistrationPageComponent', () => {
+  let component: RegistrationPageComponent;
+  let fixture: ComponentFixture<RegistrationPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AuthComponent ]
+      declarations: [ RegistrationPageComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AuthComponent);
+    fixture = TestBed.createComponent(RegistrationPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
