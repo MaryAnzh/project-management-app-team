e7a9ca2c1fda1cabfed116e6eeffd8ab40d79e0e
MaryAnzh <MaryAnzh@mail.ru>
2022-04-27 14:39:07 +0400

    feat: add page 404 template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'project-management', loadChildren: () => import('./project-management/project-management.module').then(m => m.ProjectManagementModule) }];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'project-management',
+    loadChildren: () => import('./project-management/project-management.module').then(m => m.ProjectManagementModule)
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
