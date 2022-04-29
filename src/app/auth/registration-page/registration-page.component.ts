d0f53bd70c26aaa3bdbd6735b676a741ef44aff7
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-04-29 23:48:48 +0300

    feat: add validators to registration-page

--- src/app/auth/registration-page/registration-page.component.ts
+++ src/app/auth/registration-page/registration-page.component.ts
@@ -1,14 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { MyValidators } from '../my-validators';
 
 @Component({
   selector: 'app-registration-page',
   templateUrl: './registration-page.component.html',
   styleUrls: ['./registration-page.component.scss']
 })
-export class RegistrationPageComponent {
+export class RegistrationPageComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  loginForm!: FormGroup;
+
+  passwordValue: string = 'asd'
+
+  constructor(private router: Router, private auth: AuthService) { }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      userName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2)
+      ]),
+      email: new FormControl('', [Validators.email, Validators.required
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+        MyValidators.digitValidator,
+        MyValidators.lowercaseValidator,
+        MyValidators.uppercaseValidator,
+        MyValidators.symbolsValidator,
+      ]),
+      confirmPassword: new FormControl(null, [
+        Validators.required,
+      ]),
+    },
+      {
+        validators: (control) => {
+          if (control.value.password !== control.value.confirmPassword) {
+            (<AbstractControl>control.get('confirmPassword')).setErrors({ notSame: true });
+          }
+          return null
+        }
+      }
+    );
+  }
+
+  get userName(): AbstractControl {
+    return <AbstractControl>this.loginForm.get('userName');
+  }
+
+  get email(): AbstractControl {
+    return <AbstractControl>this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl {
+    return <AbstractControl>this.loginForm.get('password');
+  }
+
+  get confirmPassword(): AbstractControl {
+    return <AbstractControl>this.loginForm.get('confirmPassword');
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      // const formData = this.loginForm.value;
+      // console.log(formData);
+      this.auth.login();
+      this.loginForm.reset();
+    }
+  }
 
   goToLoginPage(): void {
     this.router.navigate(['/auth/login']);
