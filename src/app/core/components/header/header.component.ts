d535198173fc878b4db28aaf0e049fea8aeb0791
MaryAnzh <MaryAnzh@mail.ru>
2022-04-30 10:45:08 +0400

    feat: add style

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 
 export class HeaderComponent {  }
