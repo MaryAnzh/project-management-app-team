8f8562bc543bd48f41d6248e61fe8d338eeca389
MaryAnzh <MaryAnzh@mail.ru>
2022-05-02 11:11:28 +0400

    feat: add team-info component

---
+++ src/app/information/components/team-info/team-info.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-info',
+  templateUrl: './team-info.component.html',
+  styleUrls: ['./team-info.component.scss']
+})
+export class TeamInfoComponent {  }
