eb6a2e58b59c6aabfdffbe7b47a12fbd83ece4fc
MaryAnzh <MaryAnzh@mail.ru>
2022-05-04 11:12:09 +0400

    feat: add animation

---
+++ src/app/auth/components/pop-up-auth-error/pop-up-auth-error.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up-auth-error',
+  templateUrl: './pop-up-auth-error.component.html',
+  styleUrls: ['./pop-up-auth-error.component.scss']
+})
+
+export class PopUpAuthErrorComponent {
+
+}
