db6502807c195f91cb5585b3f2750f0fa6df8ab9
MaryAnzh <MaryAnzh@mail.ru>
2022-05-06 17:14:33 +0400

    feat: sticky header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Observable, SubscriptionLike } from 'rxjs';
+import { AuthService } from './auth/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'project-management-app';
+  public isScroll: boolean = false;
+  public isScrollUp: boolean = false;
+
+  public isAuth: boolean = false;
+  public isAuth$: SubscriptionLike;
+
+  public title: string = 'project-management-app';
+
+  constructor(private authService: AuthService) {
+    this.isAuth$ = this.authService.isLoggedIn$.subscribe(
+      (value: boolean) => {
+        this.isAuth = value
+      }
+    )
+  }
+
+  @HostListener("window:scroll", []) onWindowScroll() {
+
+    const pageYOffset = window.pageYOffset;
+    if (this.isAuth) {
+      if (pageYOffset > 99) {
+        this.isScroll = true;
+      }
+      if (pageYOffset === 0) {
+        this.isScroll = false;
+      }
+    }
+
+  }
+
 }
