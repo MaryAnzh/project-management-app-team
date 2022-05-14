ea15da3834cb0a32c287443cfc288d1457be2c07
MaryAnzh <MaryAnzh@mail.ru>
2022-05-14 21:39:58 +0400

    feat: add button wrap

--- src/app/project-management/components/modal-window-task/modal-window-task.component.ts
+++ src/app/project-management/components/modal-window-task/modal-window-task.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PMDataService } from '../../services/PMData/pmdata.service';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-modal-window-task',
   templateUrl: './modal-window-task.component.html',
   styleUrls: ['./modal-window-task.component.scss']
 })
-export class ModalWindowTaskComponent implements OnInit {
+export class ModalWindowTaskComponent {
 
-  constructor() { }
+  constructor(
+    private pMDataService: PMDataService,
+    public translate: TranslateService,
+  ) {
 
-  ngOnInit(): void {
-  }
+    translate.addLangs(['en', 'ru']);
+    translate.setDefaultLang('en');
+   }
 
+
+  createTask() { }
+
+  closeModalWindow() {
+    this.pMDataService.closeModalWindowNewTask();
+}
 }
