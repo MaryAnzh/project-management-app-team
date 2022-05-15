c63c48c2e52f7c9f5d9af967fe20af63e752b2eb
MaryAnzh <MaryAnzh@mail.ru>
2022-05-15 15:23:51 +0400

    feat: add FormGroup

--- src/app/project-management/components/modal-window-task/modal-window-task.component.ts
+++ src/app/project-management/components/modal-window-task/modal-window-task.component.ts
@@ -1,20 +1,32 @@
 import { Component } from '@angular/core';
 import { PMDataService } from '../../services/PMData/pmdata.service';
 import { TranslateService } from '@ngx-translate/core';
-import { Observable } from 'rxjs';
+import { FormGroup, FormControl, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-window-task',
   templateUrl: './modal-window-task.component.html',
   styleUrls: ['./modal-window-task.component.scss']
 })
+
 export class ModalWindowTaskComponent {
+  newTaskForm = new FormGroup({
+    title: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(30),
+    ]),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(100),
+    ]),
+    doneCheck: new FormControl(''),
+    selectColumn: new FormControl(''),
+  });
 
   constructor(
     private pMDataService: PMDataService,
     public translate: TranslateService,
   ) {
-
     translate.addLangs(['en', 'ru']);
     translate.setDefaultLang('en');
    }
